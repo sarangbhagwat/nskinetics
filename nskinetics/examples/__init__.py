c87c354360c272c505acf34e2e4f93d01acc356a
sarangbhagwat <sarangb2@illinois.edu>
2025-07-01 03:34:37 -0700

    cleanup examples and add missing init file

---
+++ nskinetics/examples/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# NSKinetics: simulation of Non-Steady state enzyme Kinetics and inhibitory phenomena
+# Copyright (C) 2025-, Sarang S. Bhagwat <sarangbhagwat.developer@gmail.com>
+# 
+# This module is under the MIT open-source license. See 
+# https://github.com/sarangbhagwat/nskinetics/blob/main/LICENSE
+# for license details.
+
+from . import example_1_README
+from .example_1_README import *
+
+from . import example_2_README
+from .example_2_README import *
+
+from . import example_3_README
+from .example_3_README import *
+
+from . import example_4_README
+from .example_4_README import *
+
+
+__all__ = (
+     *example_1_README.__all__,
+     *example_2_README.__all__,
+     *example_3_README.__all__,
+     *example_4_README.__all__,
+     )
