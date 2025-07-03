b6d964780f7e4c82c96621ab73daef59ab11a757
sarangbhagwat <sarangb2@illinois.edu>
2025-07-02 20:02:51 -0700

    remove old MichaelisMenten class and add inverse modeling for Michaelis Menten instead

--- nskinetics/reaction_systems/steady.py
+++ nskinetics/reaction_systems/steady.py
@@ -8,123 +8,79 @@
 
 import numpy as np
 from numba import njit
+from scipy.optimize import curve_fit
+from pandas import DataFrame, read_excel
+from typing import Union
 
-__all__ = ('MichaelisMenten',)
+__all__ = ('michaelis_menten', 'fit_michaelis_menten')
 
 #%% Michaelis Menten (STEADY-state only)
-# (Do NOT use in non-steady state systems)
 
 @njit(cache=True)
-def steady_MM_dconcs_dt(kcat, KM, species_concs_vector, 
-                        e_index, s_index,
-                        rxn_stoichs):
-    # for a Michaelis-Menten RxnSys (STEADY-state),
-    # returns array of temporal rates of change 
-    # in the concentrations of given species
-    # (- denotes decrease, + denotes increase)
-    s_conc = species_concs_vector[s_index]
-    change = kcat*species_concs_vector[e_index]*species_concs_vector[s_index]/(s_conc + KM)
-    temp_vector = np.copy(species_concs_vector)
-    temp_vector += change*rxn_stoichs
-    # breakpoint()
-    if np.any(temp_vector<0):
-        tv_stoich_adj = temp_vector/np.abs(rxn_stoichs)
-        tv_stoich_adj[np.where(np.isinf(tv_stoich_adj))] = 0.
-        tv_stoich_adj[np.where(np.isnan(tv_stoich_adj))] = 0.
-        limiting_reactant_index = np.where(tv_stoich_adj==np.min(tv_stoich_adj))[0][0]
-        # print(limiting_reactant_index)
-        change = species_concs_vector[limiting_reactant_index]/np.abs(rxn_stoichs[limiting_reactant_index]) # limiting reactant conc. adjusted by stoichiometry
-    return change * rxn_stoichs
+def michaelis_menten(S, Vmax, KM):
+    return Vmax * S / (KM + S)
 
-class MichaelisMenten():
+def fit_michaelis_menten(data: Union[DataFrame, dict, str],
+                         E0: float,
+                         s_col: str = 'S',
+                         v_col: str = 'V',
+                         sheet_name: Union[str, int] = 0,
+                         S_data=None,
+                         V_data=None):
     """
-    Abstract class for a STEADY-STATE system of 
-    reactions involving a single enzyme, 
-    substrate, and product.
+    Fit Michaelis-Menten parameters (kcat, KM) from data in a DataFrame, 
+    dict, or Excel file.
     
-    Parameters
-    ----------
-    ID : str
-        ID.
-    enzyme : str
-        Enzyme.
-    substrate : str
-        Substrate.
-    product : str
-        Product.
+    Parameters:
+    -----------
+    data : DataFrame, dict, or str
+        Input data:
+        - pandas DataFrame with columns s_col and v_col
+        - dict with keys s_col and v_col
+        - Excel file path with columns s_col and v_col
+    
+    E0 : float
+        Total enzyme concentration [E]_0
+        
+    s_col : str
+        Column name for substrate concentration [S]
+    
+    v_col : str
+        Column name for reaction rate V
+        
+    sheet_name : str or int
+        Sheet name/index to read if loading from Excel
+    
+    Returns:
+    --------
     kcat : float
-        Rate constant for the dissociation of
-        enzyme-substrate complex to form enzyme
-        and product.
-    KM: float.
-        Michaelis constant. Equals (koff + kcat)/kon.
-    species_system : SpeciesSystem
-        A SpeciesSystem object containing all species
-        involved in this system of reactions.
+        Turnover number (units of V / E0)
+        
+    KM : float
+        Michaelis constant (same units as S)
     
     """
-    def __init__(self, 
-                 ID, 
-                 enzyme, 
-                 substrate, 
-                 product,
-                 kcat, KM,
-                 species_system):
-        self.ID = ID
-        self.enzyme = enzyme
-        self.substrate = substrate
-        self.product = product
-        self.kcat = kcat
-        self.KM = KM
-        all_sps_IDs = [i.ID for i in species_system.all_sps]
-        self._enzyme_index = all_sps_IDs.index(enzyme)
-        self._substrate_index = all_sps_IDs.index(substrate)
-        # self.product_index = all_sps.index(product)
-        self.species_system = species_system
-        
-        stoichiometry = []
-        
-        reactants = {}
-        if isinstance(substrate, dict):
-            reactants.update(substrate)
+    if S_data is None:
+        if isinstance(data, DataFrame):
+            df = data
+        elif isinstance(data, dict):
+            df = DataFrame(data)
+        elif isinstance(data, str):
+            df = read_excel(data, sheet_name=sheet_name)
         else:
-            reactants = [substrate]
-        
-        products = {}
-        if isinstance(product, dict):
-            products.update(product)
-        else:
-            products = [product]
- 
-        if isinstance(reactants, dict) and isinstance(products, dict):
-            reactant_keys = reactants.keys()
-            product_keys = products.keys()
-            for i in species_system.all_sps:
-                if i.ID in reactant_keys: 
-                    stoichiometry.append(reactants[i])
-                elif i.ID in product_keys: 
-                    stoichiometry.append(products[i])
-                else:
-                    stoichiometry.append(0.)
-        elif isinstance(reactants, list) and isinstance(products, list):
-            for i in species_system.all_sps:
-                if i.ID in reactants: 
-                    stoichiometry.append(-1)
-                elif i.ID in products: 
-                    stoichiometry.append(1)
-                else:
-                    stoichiometry.append(0.)
-        # else:
-        #     raise TypeError('\nGiven reactants and products must both be either lists or dicts.\n')
-        
-        self.stoichiometry = np.array(stoichiometry)
-        
-    def get_dconcs_dt(self):
-        return steady_MM_dconcs_dt(kcat=self.kcat, 
-                             KM=self.KM,
-                             e_index=self._enzyme_index, 
-                             s_index=self._substrate_index,
-                             species_concs_vector=self.species_system.concentrations, 
-                             rxn_stoichs=self.stoichiometry)
+            raise TypeError("Data must be a DataFrame, dict, or Excel file path.")
     
-SteadyStateCompetitiveInhibition = MichaelisMenten
+        if s_col not in df or v_col not in df:
+            raise ValueError(f"Columns '{s_col}' and '{v_col}' must be in the data.")
+    
+        S_data = df[s_col].to_numpy(dtype=float)
+        V_data = df[v_col].to_numpy(dtype=float)
+    
+    # Initial guesses: Vmax=max(V), KM=median(S)
+    p0 = [np.max(V_data), np.median(S_data)]
+    
+    popt, _ = curve_fit(michaelis_menten, S_data, V_data, p0=p0)
+    Vmax_fit, KM_fit = popt
+    kcat_fit = Vmax_fit / E0
+    
+    return kcat_fit, KM_fit
