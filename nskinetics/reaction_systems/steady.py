160e02e7cd610597629a0ea715dc8db068a861c1
sarangbhagwat <sarangb2@illinois.edu>
2025-07-02 20:17:10 -0700

    add optional normalization to michaelis menten inverse modeling

--- nskinetics/reaction_systems/steady.py
+++ nskinetics/reaction_systems/steady.py
@@ -25,6 +25,7 @@ def fit_michaelis_menten(data: Union[DataFrame, dict, str],
                          s_col: str = 'S',
                          v_col: str = 'V',
                          sheet_name: Union[str, int] = 0,
+                         normalize=False,
                          S_data=None,
                          V_data=None):
     """
@@ -76,11 +77,31 @@ def fit_michaelis_menten(data: Union[DataFrame, dict, str],
         S_data = df[s_col].to_numpy(dtype=float)
         V_data = df[v_col].to_numpy(dtype=float)
     
-    # Initial guesses: Vmax=max(V), KM=median(S)
-    p0 = [np.max(V_data), np.median(S_data)]
+    S_data_norm, V_data_norm = None, None
+    if normalize:
+        S_scale = np.max(S_data)
+        S_data_norm = S_data/S_scale
+        V_scale = np.max(V_data)
+        V_data_norm = V_data/V_scale
+        
     
-    popt, _ = curve_fit(michaelis_menten, S_data, V_data, p0=p0)
+    S_data_for_fit = S_data if not normalize else S_data_norm
+    V_data_for_fit = V_data if not normalize else V_data_norm
+    
+    # Initial guesses: Vmax=max(V), KM=median(S)
+    p0 = [np.max(V_data_for_fit), np.median(S_data_for_fit)]
+    
+    popt, _ = curve_fit(michaelis_menten, 
+                        S_data_for_fit, 
+                        V_data_for_fit, 
+                        p0=p0)
+    
     Vmax_fit, KM_fit = popt
+    
+    if normalize:
+        Vmax_fit *= V_scale
+        KM_fit *= S_scale
+    
     kcat_fit = Vmax_fit / E0
     
     return kcat_fit, KM_fit
