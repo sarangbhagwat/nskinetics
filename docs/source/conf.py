98e199e4a4f516b8c14d533b91cbe224ade7b521
sarangbhagwat <sarangb2@illinois.edu>
2025-07-03 23:54:18 -0700

    fix logo file name typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ html_static_path = ['_static']
 html_theme_options = {
     "logo" : {
         'image_light': '_static/images/logo/logo_nskinetics.png',
-        'image_dark': '_static/images/logo/logo_nskinetics_dark.png'
+        'image_dark': '_static/images/logo/logo_dark_nskinetics.png'
     },
     "show_toc_level": 2,
 #     "announcement": (
