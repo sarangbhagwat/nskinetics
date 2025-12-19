3c2f14e05e4fd6a59fa1cb5664f44c4e6938b7df
sarangbhagwat <sarangb2@illinois.edu>
2025-12-19 12:35:28 -0800

    fix incorrect CR

--- nskinetics/units/_feeding_strategy_specification.py
+++ nskinetics/units/_feeding_strategy_specification.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Bioindustrial-Park: BioSTEAM's Premier Biorefinery Models and Results
-# Copyright (C) 2025-, Sarang Bhagwat <sarangb2@illinois.edu>
+# NSKinetics: simulation of Non-Steady state enzyme Kinetics and inhibitory phenomena
+# Copyright (C) 2025-, Sarang S. Bhagwat <sarangbhagwat.developer@gmail.com>
 # 
-# This module is under the UIUC open-source license. See 
-# github.com/BioSTEAMDevelopmentGroup/biosteam/blob/master/LICENSE.txt
+# This module is under the MIT open-source license. See 
+# https://github.com/sarangbhagwat/nskinetics/blob/main/LICENSE
 # for license details.
 
 import nskinetics as nsk
